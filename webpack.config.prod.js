1d58a60142d2cc5a7b8105b3613ffbcac8996a34
Vincent Cotro <vincentcotro@hec.edu>
2016-12-13 14:31:04 +0100

    test2

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,13 +1,14 @@
 var path = require("path");
 var webpack = require("webpack");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const serverURL = process.env.SERVER_URL || "https://kinto.notmyidea.org/v1/";
-const appURL = process.env.APP_URL || "https://www.fourmilieres.net/";
-
+const serverURL = process.env.SERVER_URL || "http://localhost:8888/v1/";
 
 module.exports = {
-  entry: ["./formbuilder/app", "babel-polyfill"],
+  devtool: "eval",
+  entry: [
+    "webpack-hot-middleware/client?reload=true",
+    "./formbuilder/app"
+  ],
   output: {
     path: path.join(__dirname, "build"),
     filename: "bundle.js",
@@ -15,18 +16,14 @@ module.exports = {
   },
   plugins: [
     new webpack.IgnorePlugin(/^(buffertools)$/), // unwanted "deeper" dependency
-    new ExtractTextPlugin("styles.css", {allChunks: true}),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       "process.env": {
-        NODE_ENV: JSON.stringify("production"),
-        SERVER_URL: JSON.stringify(serverURL),
-        APP_URL: JSON.stringify(appURL),
-      },
+        SERVER_URL: JSON.stringify(serverURL)
+      }
     })
   ],
-  resolve: {
-    extensions: ["", ".js", ".jsx", ".css"]
-  },
   module: {
     loaders: [
       {
@@ -36,10 +33,22 @@ module.exports = {
           path.join(__dirname, "src"),
           path.join(__dirname, "formbuilder"),
         ],
+        query: {
+          plugins: ["react-transform"],
+          extra: {
+            "react-transform": {
+              transforms: [{
+                transform: "react-transform-hmr",
+                imports: ["react"],
+                locals: ["module"]
+              }]
+            }
+          }
+        }
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract("css-loader"),
+        loader: "style!css",
         include: [
           path.join(__dirname, "css"),
           path.join(__dirname, "formbuilder"),
